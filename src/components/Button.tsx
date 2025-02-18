9efdce8bf1aff518a84e26a20d0ac524bd13709c
Stephen Meriwether <stephen@merimerimeri.com>
2025-02-18 14:41:26 -0500

    WIP

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -42,5 +42,266 @@ export function Button({
     </button>
   );
 }
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
+export function Button({
+  children,
+  onClick,
+  active = false,
+  className,
+  style,
+}: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onMouseDown={(evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }}
+      onClick={onClick}
+      className={cn(
+        className,
+        'text-base leading-tight font-bold rounded-full py-2 px-4 focus:outline focus:outline-offset-2 focus:outline-link dark:focus:outline-link-dark inline-flex items-center my-1',
+        {
+          'bg-link border-link text-white hover:bg-link focus:bg-link active:bg-link':
+            active,
+          'bg-transparent text-primary dark:text-primary-dark active:text-primary shadow-secondary-button-stroke dark:shadow-secondary-button-stroke-dark hover:bg-gray-40/5 active:bg-gray-40/10  hover:dark:bg-gray-60/5 active:dark:bg-gray-60/10':
+            !active,
+        }
+      )}>
+      {children}
+    </button>
+  );
+}
 
 export default Button;
